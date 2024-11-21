a0f4704adf2ea7118b957e6f0466c324d2316c46
HEDO <hedo@oocl.com>
2024-11-21 13:56:51 +0800

    refactor: teacher extends Person

--- src/main/java/oo/Teacher.java
+++ src/main/java/oo/Teacher.java
@@ -1,37 +1,16 @@
 package oo;
 
-import java.util.Objects;
-
-public class Teacher {
-    private Integer id;
-
-    private String name;
-
-    private Integer age;
+public class Teacher extends Person{
 
     public Teacher() {
     }
 
     public Teacher(Integer id, String name, Integer age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
+        super(id, name, age);
     }
 
+    @Override
     public String introduce() {
         return String.format("My name is %s. I am %d years old. I am a teacher.", name, age);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Teacher teacher = (Teacher) o;
-        return Objects.equals(id, teacher.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
     }
 }
