25e41d5fd8d2090d9c29e31a4586708f467cd271
HEDO <hedo@oocl.com>
2024-11-21 14:44:29 +0800

    feat: add klass isLeader method

--- src/main/java/oo/Klass.java
+++ src/main/java/oo/Klass.java
@@ -5,7 +5,12 @@ import java.util.Objects;
 public class Klass {
 
     public static final Integer UNDEFINED_CLASS_NUMBER = Integer.MIN_VALUE;
+
+    private static final String STUDENT_NOT_IN_CLASS = "it is not one of us";
+
     private Integer number;
+
+    private Student leader;
 
     public Klass() {
         this.number = UNDEFINED_CLASS_NUMBER;
@@ -17,6 +22,21 @@ public class Klass {
 
     public Integer getNumber() {
         return number;
+    }
+
+    public void assignLeader(Student student) {
+        if (student == null || student.isIn(this)) {
+            System.out.println(STUDENT_NOT_IN_CLASS);
+            return;
+        }
+        leader = student;
+    }
+
+    public boolean isLeader(Student student) {
+        if (leader == null || student == null) {
+            return false;
+        }
+        return leader.equals(student);
     }
 
     @Override
