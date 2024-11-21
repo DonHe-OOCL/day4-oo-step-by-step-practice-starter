f166a8455b2dd5d08163e27938a2bf445f01a8b8
HEDO <hedo@oocl.com>
2024-11-21 13:41:16 +0800

    feat: judge person by id

--- src/main/java/oo/Person.java
+++ src/main/java/oo/Person.java
@@ -21,4 +21,17 @@ public class Person {
     public String introduce() {
         return String.format("My name is %s. I am %d years old.", name, age);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
