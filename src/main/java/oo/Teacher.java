e9d96f4663b4953f8bdd2275fd1d6b01e41e11a0
HEDO <hedo@oocl.com>
2024-11-21 13:50:05 +0800

    feat: add teacher entity

--- src/main/java/oo/Teacher.java
+++ src/main/java/oo/Teacher.java
@@ -1,4 +1,24 @@
 package oo;
 
+import java.util.Objects;
+
 public class Teacher {
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    public Teacher() {
+    }
+
+    public Teacher(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old. I am a teacher.", name, age);
+    }
 }
