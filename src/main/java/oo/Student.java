f2c6a1f1da5fc8c43a2a5ae09432679feff061dc
HEDO <hedo@oocl.com>
2024-11-21 13:46:35 +0800

    feat: judge student by id

--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -21,4 +21,17 @@ public class Student {
     public String introduce() {
         return String.format("My name is %s. I am %d years old. I am a student.", name, age);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
