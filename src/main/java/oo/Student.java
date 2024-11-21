a76cd1806300ec74a6ef30a0216e5a2a3c564723
HEDO <hedo@oocl.com>
2024-11-21 13:44:41 +0800

    feat: add student entity

--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -1,4 +1,24 @@
 package oo;
 
+import java.util.Objects;
+
 public class Student {
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    public Student() {
+    }
+
+    public Student(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old. I am a student.", name, age);
+    }
 }
