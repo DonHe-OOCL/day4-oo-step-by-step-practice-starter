5aa95a4c3293cef6501945a36d3cd82b21606a4e
HEDO <hedo@oocl.com>
2024-11-21 13:56:12 +0800

    refactor: student extends Person

--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -1,37 +1,17 @@
 package oo;
 
-import java.util.Objects;
 
-public class Student {
-    private Integer id;
-
-    private String name;
-
-    private Integer age;
+public class Student extends Person{
 
     public Student() {
     }
 
     public Student(Integer id, String name, Integer age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
+        super(id, name, age);
     }
 
+    @Override
     public String introduce() {
         return String.format("My name is %s. I am %d years old. I am a student.", name, age);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return Objects.equals(id, student.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
     }
 }
